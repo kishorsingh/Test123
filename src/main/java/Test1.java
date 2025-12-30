1f8f5b29ef21c8ee2ec474caa400913e8ce85cfb
Gabbar <gabbar@Gabbars-MacBook-Pro.local>
2025-12-30 14:48:04 +0530

    Initial commit

---
+++ src/main/java/Test1.java
@@ -0,0 +1,8 @@
+public class Test1 {
+    public static void main(String args[]){
+        System.out.println("This is first program");
+        for(int i=0; i<10;i++){
+            System.out.println("This is number : "+ i);
+        }
+    }
+}
